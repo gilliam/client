cac88d13de06c381bfb0dfbc7e32255ffb5827d3
Johan Rydberg <johan.rydberg@gmail.com>
2013-01-30 21:21:07 +0100

    initial work

---
+++ README.md
@@ -0,0 +1,102 @@
+# The Gilliam Client
+
+This is the `gilliam` CLI client for the Gilliam app platform.  It is a
+simple and pretty straight format tool to interact with the system.
+
+Hopefully the commands are intuitive.
+
+# Installation
+
+To be written.
+
+# Config Files
+
+To be written
+
+
+# Basic Commands
+
+Here we go through the basic commands, all of which has not yet been
+implemented.  Work in progress, as usual.
+
+## Create an Application
+
+This command creates a new "application" in the orchestrator.  You
+give it a name (that must be unique) and then a URL where the code can
+be found.
+
+    $ gilliam create example-app https://github.com/gilliam/python-example.git
+
+This does not build anything.  It just registers the application and
+the source URL for future use.
+
+This command sets the *app file* `.gilliam.app` in the current
+directory to point to the newly created app.
+
+
+## Building an Image
+
+Before you can do your first deploy you must build your application
+code into something that can be deployed.  Simply issue the `build`
+command to build your `master` branch.
+
+    $ gilliam build
+    ...
+    
+
+It is possible to specify a commit (tag, branch or hash) if you want
+to build something special from the repository:
+
+    $ gilliam build hot-fix-branch-2
+    ...
+ 
+Each build receive a name. Use this name when you refer to your build
+when you want to deploy it.
+
+## Deploying
+
+A deploy is the union of a build and configuration.  It is not
+possible to change build without doing a new deploy and the same is
+true for the configuration.
+
+The basics of the deploy command is simple:
+
+    $ gilliam deploy [build] [config params]
+
+So for example, to deploy build called `6dc2f9e` with two config
+parameters; `POOL_SIZE=10` and `CONN_LIMIT=100`, issue the following
+command:
+
+    $ gilliam deploy 6dc2f9e POOL_SIZE=10 CONN_LIMIT=100
+
+If something is already deployed then those settings will be inherited
+to the next invokation of `deploy`.  Say that you just wanna change
+the `POOL_SIZE` setting to 16:
+
+    $ gilliam deploy POOL_SIZE=16
+
+This will result in a new deploy still with build `6dc2f9e` and
+`CONN_LIMIT` set to 100, but now with a pool size of 16.
+
+Issuing the command without any parameters will display some
+information about the current deploy.
+
+## Displaying Configuration
+
+Just issue the `gilliam config` command to display the current full
+configuration.
+
+
+## Setting Scale Values for an Application
+
+The number of instances for an application process is controlled using
+the `scale` command:
+
+    $ gilliam scale web=4
+
+Besides setting absolute values it is possible to give a delta to the
+current scale.
+
+    $ gilliam scale web=+1
+
+(It is of course possible to give negative deltas.)
---
+++ xgilliam/client.py
@@ -0,0 +1,255 @@
+# Copyright 2013 Johan Rydberg.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Usage: gilliam [--version] <command> [<args>...]
+
+Commands:
+    build       Issue a new build
+    deploy      Deploy a new build or configuration
+    config      Show current configuration
+    status      Display current status for application
+    create      Create a new application.
+    attach      Attach environment to existing application
+
+See `gilliam help <command>` for more information on a specific command.
+
+"""
+
+import sys
+from docopt import docopt
+import os.path
+import yaml
+import requests
+import json
+
+
+def urlchild(base_url, *args):
+    return base_url + ''.join([('/%s' % arg) for arg in args])
+
+
+COMMANDS = {}
+
+def expose(name):
+    """Expose a function as a command."""
+    def decl(f):
+        COMMANDS[name] = f
+        return f
+    return decl
+
+
+class Config(object):
+    """Wraps our configuration files.
+
+    There are two files.  One YAML file called `.gilliam` living in
+    the home directory.  This file contains pointers to the
+    orchestrator and the build server.  The second file `.gilliam.app`
+    is just a simple text file with the name of the current app.
+    """
+
+    CONF = '.gilliam'
+    ATTACH = '.gilliam.app'
+
+    def __init__(self):
+        self._config = None
+        self._attach = None
+
+    def _read_config(self):
+        for path in (self.CONF, os.path.expanduser('~/' + self.CONF)):
+            if os.path.exists(path):
+                with open(path) as fp:
+                    self._config = yaml.load(fp)
+                break
+        else:
+            raise RuntimeError("No config file")
+    
+    def _read_attach(self):
+        if self._attach is None:
+            if not os.path.exists(self.ATTACH):
+                raise RuntimeError("Not attached to an app")
+            with open(self.ATTACH) as fp:
+                self._attach = fp.read().strip()
+        return self._attach
+
+    @property
+    def app_url(self):
+        """Return base URL to the attached app in the orchestrator."""
+        attach = self._read_attach()
+        return urlchild(self.orch_url, 'app', attach)
+        
+    @property
+    def orch_url(self):
+        """Return base URL to the orchestrator."""
+        self._read_config()
+        return 'http://%s:%d' % (self._config['orchestrator']['host'],
+                                  self._config['orchestrator']['port'])
+
+
+class API(object):
+    """Abstraction that provides functions to talk to the orchestrator
+    using its REST API.
+    """
+
+    def __init__(self, config, requests):
+        self.config = config
+        self.requests = requests
+
+    def _get_json(self, *parts):
+        try:
+            response = self.requests.get(urlchild(*parts))
+            response.raise_for_status()
+        except:
+            raise
+        else:
+            return response.json()
+
+    def _put_json(self, data, *parts):
+        """Store data using a PUT request."""
+        try:
+            response = self.requests.put(urlchild(*parts),
+                                         data=json.dumps(data))
+            response.raise_for_status()
+        except:
+            raise
+
+    def scale(self):
+        return self._get_json(self.config.app_url, 'scale')
+
+    def set_scale(self, scale):
+        self._put_json(scale, self.config.app_url, 'scale')
+
+    def deploy(self):
+        """Return current deploy as a C{dict}."""
+        return self._get_json(self.config.app_url, 'deploy', 'latest')
+
+    def create_deploy(self, build, image, pstable,
+                      app_config, text):
+        try:
+            request = {'build': build, 'image': image,
+                       'pstable': pstable, 'config': app_config,
+                       'text': text}
+            response = self.requests.post(urlchild(
+                self.config.app_url, 'deploy'),
+                data=json.dumps(request))
+            response.raise_for_status()
+        except:
+            raise
+        else:
+            return response.json()
+
+
+@expose("attach")
+def attach(config, app_options, argv, requests=requests):
+    """Usage: gilliam attach APP
+
+    Attach to a given app.
+    """
+    options = docopt(attach.__doc__, argv=argv)
+    app_url = urljoin(config.orch_url, '/app/%s' % (options['APP'],))
+    try:
+        response = requests.get(app_url)
+    except RequestException:
+        raise
+    else:
+        if response.status == 400:
+            sys.exit("%s: %s: no such app" % (argv[0], options['APP']))
+        else:
+            sys.exit("%s: server response: %r" % (argv[0], response))
+        output = response.json()
+        config.attach(options['APP'])
+
+
+@expose("scale")
+def scale(config, orch_api, app_options, argv):
+    """Usage: gilliam scale [<SPEC>...]
+
+    Set scale parameters for procs.
+
+    The SPEC is specified as a proc name and scale value, like
+    `web=20`.  It is possible to increase or decrease the scale value
+    using a `+` or `-` prefix.  For example `web=+2` will increase the
+    scale for the web proc with two instances.
+    """
+    options = docopt(scale.__doc__, argv=argv)
+    deploy = orch_api.deploy()
+    current = orch_api.scale()
+    if options['<SPEC>']:
+        for spec in options['<SPEC>']:
+            proc, value = spec.split('=', 1)
+            if proc not in deploy['pstable']:
+                sys.exit("%s: %s: no such proc according to deploy" % (
+                        argv[0], proc))
+            if value[0] in ('+', '-'):
+                current[proc] = max(0, current.get(proc, 0) + int(value))
+            else:
+                current[proc] = int(value)
+        orch_api.set_scale(current)
+    for proc, value in current.items():
+        print "%s=%d" % (proc, value)
+
+
+@expose("deploy")
+def deploy(config, orch_api, app_options, argv):
+    """Usage: gilliam deploy [options] [BUILD] [CONFIG...]
+
+    Options:
+        -m, --message <message>   Deploy message
+
+    """
+    options = docopt(deploy.__doc__, argv=argv)
+    argv = filter(None, [options['BUILD']] + options['CONFIG'])
+    current = orch_api.deploy()
+    if not argv:
+        print "deploy %d: %s at %s: %s" % (current['id'],
+            current['build'], current['when'], current['text'])
+        return
+    elif not '=' in argv[0]:
+        options['BUILD'], argv = argv[0], argv[1:]
+    else:
+        options['BUILD'] = None
+    build = options['BUILD'] or current['build']
+    # update config with new settings if needed:
+    app_config = current['config']
+    for pair in argv:
+        name, value = pair.split('=', 1)
+        app_config[name] = value
+    # FIXME: at this point we know the build and the config.  talk to
+    # the buildserver to get hold of a URL to the image and the
+    # pstable.
+    pstable = {'web': 'pythob web.py'}
+    image = 'app.tar.gz'
+    message = options['--message'] or ('build %s%s' % (
+            build, (' with config changes' if len(argv) else '')))
+    orch_api.create_deploy(build, image, pstable, app_config, message)
+
+
+@expose("help")
+def help(app_options, argv):
+    """."""
+    # FIXME: todo.
+    print "Help", args
+
+
+def main():
+    options = docopt(__doc__, version='gilliam 0.0', options_first=True)
+    command = options.get('<command>', 'help')
+    if command not in COMMANDS:
+        sys.exit("Unknown command")
+    config = Config()
+    orch_api = API(config, requests)
+    try:
+        COMMANDS[command](config, orch_api, options,
+                          [command] + options['<args>'])
+    except RuntimeError, re:
+        sys.exit(str(re))

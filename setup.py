94687c5dc3ce7afeb61373ff4f19e8ca39dff988
Johan Rydberg <johan.rydberg@gmail.com>
2013-10-08 23:50:56 +0200

    requests 2.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     install_requires=[
         'PyYAML==3.10',
         'docopt==0.6.0',
-        'requests==1.1.0',
+        'requests==2.0',
         'python-dateutil==2.1',
         "gilliam-py",
         "shortuuid"
